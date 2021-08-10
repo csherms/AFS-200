b990da18df5ec59b8c9c9747e40bf4cc44a46a74
csherms <casey.sherman@bryanuniversity.edu>
2021-08-10 14:42:58 -0600

    first_commit

---
+++ week_2/Class/class.py
@@ -0,0 +1,3 @@
+# Class
+user_input = input("What's your favourite language? ")
+print("My favourite language is ", user_input.upper())
---
+++ week_2/Variable/variable.py
@@ -0,0 +1,5 @@
+# Variables
+names = ["John", "Casey", "Kelley"];
+subjects = ["Math", "Science", "English"]
+print("Your son", names[0], "is failing", subjects[0], "and", subjects[1]);
+print(names[0], "will need to redo", subjects[0:2])
