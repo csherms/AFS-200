4821a171f15685f421f8a1acb27f48e909886ba1
csherms <casey.sherman@bryanuniversity.edu>
2021-09-10 13:21:53 -0600

    first_commit

---
+++ week_6/Python_Projects/first_site/hello/calc/views.py
@@ -0,0 +1,14 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+  return render(request, 'home.htm', {'name':'Casey'});
+
+def add(request):
+  val1 = int(request.GET['num1'])
+  val2 = int(request.GET['num2'])
+  res = val1 + val2
+  return render(request, "result.htm", {'result':res})
